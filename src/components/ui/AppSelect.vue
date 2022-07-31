4f1e1b2dd64e680f5faddbd87b36942c9a13f47f
alinaromanova <aln.rom@mail.ru>
2022-07-31 19:13:36 +0300

    add: select input component

---
+++ src/components/ui/AppSelect.vue
@@ -0,0 +1,175 @@
+<script>
+import vClickOutside from "click-outside-vue3";
+
+export default {
+  directives: {
+    clickOutside: vClickOutside.directive,
+  },
+
+  emits: ["update:selectedOption"],
+
+  props: {
+    selectedOption: {
+      type: Object,
+    },
+
+    options: {
+      type: Array,
+      required: true,
+    },
+
+  },
+
+  data() {
+    return {
+      open: false,
+    };
+  },
+
+  computed: {
+    radioButtonValue: {
+      get() {
+        return this.selectedOption;
+      },
+      set(selectedOption) {
+        this.$emit("update:selectedOption", selectedOption);
+        this.open = false;
+      },
+    },
+  },
+
+  methods: {
+    onClickOutside() {
+      this.open = false;
+    },
+
+    showSelectorOptions() {
+        this.open = !this.open;
+    },
+  },
+};
+</script>
+
+<template>
+  <div
+      class="app-radio-select"
+  >
+
+    <div
+        class="app-radio-select__item"
+        :class="[
+        { 'app-radio-select__item_opened': open },
+      ]"
+        @click="showSelectorOptions"
+    >
+      <p v-if="selectedOption">{{ selectedOption.name }}</p>
+    </div>
+
+    <div
+        v-if="open"
+        v-click-outside="onClickOutside"
+        class="app-radio-select__popup popup"
+    >
+      <div v-for="option of options" :key="option" class="popup__item">
+        <input
+            :id="option.id"
+            v-model="radioButtonValue"
+            class="popup__radio"
+            type="radio"
+            :value="option"
+        />
+        <label :for="option.id">{{ option.name }}</label>
+      </div>
+
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import '../../assets/styles/variables.scss';
+.app-radio-select {
+  font-family: 'Source Sans Pro', sans-serif;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  position: relative;
+  width: 100%;
+
+  &__item {
+    position: relative;
+    padding: 10px 0 10px 16px;
+    height: 36px;
+    width: 100%;
+    transition: background-color 0.2s ease;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    background: #FFFEFB;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+    border-radius: 4px;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 15px;
+    color: #B4B4B4;
+
+
+    &:hover {
+      background: #FAFAFA;
+    }
+
+    &_opened {
+      &:after {
+        transform: rotate(180deg);
+      }
+    }
+  }
+
+  &__popup {
+    position: absolute;
+    top: 58px;
+    left: 0;
+    z-index: 20;
+    max-height: 250px;
+    overflow: auto;
+    background-color: $background-white;
+    box-shadow: 0 0 30px rgba(0, 0, 0, 0.1);
+    padding: 16px;
+    color: $label-primary;
+    width: 100%;
+    border-radius: 4px;
+    font-weight: 400;
+    font-size: 12px;
+  }
+
+  .popup {
+    &__item {
+      label {
+        cursor: pointer;
+        color: $label-primary;
+        display: flex;
+        align-items: center;
+        margin-bottom: 4px;
+
+        &:hover {
+          color: $label-secondary;
+        }
+      }
+
+      &:not(:last-child) {
+        margin-bottom: 8px;
+      }
+
+      p {
+        color: $label-secondary;
+      }
+    }
+
+    &__radio {
+      position: absolute;
+      opacity: 0;
+    }
+  }
+}
+</style>
